50e9e99e64faabceec71de198d2bdcfaa6bf74cc
Lakkygen <michaeladedeji366@gmail.com>
2026-07-25 16:14:15 +0100

    Refactor MainLayout component structure and props

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
-import { AnimatePresence, motion } from 'framer-motion';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 
@@ -8,26 +7,16 @@ export default function MainLayout() {
   const location = useLocation();
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-  // Close mobile menu on route change
   useEffect(() => {
     setMobileMenuOpen(false);
   }, [location.pathname]);
 
   return (
-    <div className="flex flex-col min-h-screen bg-white">
-      <Navbar mobileOpen={mobileMenuOpen} onMobileToggle={() => setMobileMenuOpen(!mobileMenuOpen)} />
-      <AnimatePresence mode="wait">
-        <motion.main
-          key={location.pathname}
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -20 }}
-          transition={{ duration: 0.3 }}
-          className="flex-1"
-        >
-          <Outlet />
-        </motion.main>
-      </AnimatePresence>
+    <div className="min-h-screen bg-white flex flex-col">
+      <Navbar mobileMenuOpen={mobileMenuOpen} setMobileMenuOpen={setMobileMenuOpen} />
+      <main className="flex-1 pt-16">
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
